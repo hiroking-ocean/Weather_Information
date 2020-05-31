dab4b09917d602382eb86c0d4309de1ec43fc6ad
wsl HIROKI <hariiniya@gmail.com>
2020-05-31 13:31:04 +0900

    Create caarousels of weather Images

--- app/helpers/weather_helper.rb
+++ app/helpers/weather_helper.rb
@@ -1,2 +1,14 @@
 module WeatherHelper
+
+  def FileNameArray(directry)
+    array = []
+    filenames = Dir::entries("app/assets/images/" + directry)
+    filenames.each do |name|
+      if (name != ".") and (name != "..")
+        print name + "\n"
+        array.push(directry + File.basename(name))
+      end
+    end
+    return array
+  end
 end
